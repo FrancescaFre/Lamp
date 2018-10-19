9a3dcf834eb7ad240c1854ca23d4db2b536dce85
Michael <michael.danielnaguib@gmail.com>
2018-10-19 22:47:32 +0200

    Improve: item selection
    now item selction works with PS4 joypad and mouse

--- Prototypes/Assets/Flancanstain/ItemWheel.cs
+++ Prototypes/Assets/Flancanstain/ItemWheel.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemWheel : MonoBehaviour {//should be attached to the game manager
     public GameObject Wheel;
@@ -8,6 +7,11 @@ public class ItemWheel : MonoBehaviour {//should be attached to the game manager
     public GameObject Item2;
     public GameObject Item3;
     public GameObject Item4;
+
+    public Button top;
+    public Button bottom;
+    public Button left;
+    public Button right;
 	// Use this for initialization
 	void Start () {
         Wheel.SetActive(false);
@@ -16,10 +20,14 @@ public class ItemWheel : MonoBehaviour {//should be attached to the game manager
         Item3.SetActive(false);
         Item4.SetActive(false);
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        //this values sets all the buttons of the wheel to be interactable
+        _ActivateWheel();
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
 
         if (Input.GetKeyDown(KeyCode.I) || Input.GetButtonDown("PS4_L1"))
             Wheel.SetActive(true);
@@ -34,6 +42,7 @@ public class ItemWheel : MonoBehaviour {//should be attached to the game manager
             print("L1 + ");
             #region Vertical Items
             //if the RIGHT stick is moved on the vertical axis, the top or bottom item is selected
+            //the use of the tresholds guarantee a correct selection even in the worst case scenario
             if (rStickX > -0.1f && rStickX < 0.1f && rStickY < -0.9f) {
                 EnableTop();
                 print("up" + rStickX + rStickY);
@@ -45,7 +54,8 @@ public class ItemWheel : MonoBehaviour {//should be attached to the game manager
             #endregion
 
             #region Horizontal Items
-            ////if the RIGHT stick is moved on the Horrizontal axis, the left or right item is selected
+            //if the RIGHT stick is moved on the Horrizontal axis, the left or right item is selected
+            //the use of the tresholds guarantee a correct selection even in the worst case scenario
             if (rStickY > -0.1f && rStickY < 0.1f && rStickX < -0.9f) {
                 EnableLeft();
                 print("Left" + rStickX + rStickY);
@@ -60,13 +70,15 @@ public class ItemWheel : MonoBehaviour {//should be attached to the game manager
     }
 
     #region Item Enable
-    /*it does not matter which one is in in which position
+    /*it does not matter which item is in in which position
      */
-   public void EnableTop() {  // this enables the top item
+    public void EnableTop() {  // this enables the top item 
         Item1.SetActive(true);
         Item2.SetActive(false);
         Item3.SetActive(false);
         Item4.SetActive(false);
+
+        _SetTopInteractable();
     }
 
     public void EnableRight() {
@@ -74,18 +86,71 @@ public class ItemWheel : MonoBehaviour {//should be attached to the game manager
         Item2.SetActive(true);
         Item3.SetActive(false);
         Item4.SetActive(false);
+
+        _SetRightInteractable();
     }
     public void EnableDown() {
         Item1.SetActive(false);
         Item2.SetActive(false);
         Item3.SetActive(true);
         Item4.SetActive(false);
+
+        _SetBottomInteractable();
     }
     public void EnableLeft() {
         Item1.SetActive(false);
         Item2.SetActive(false);
         Item3.SetActive(false);
         Item4.SetActive(true);
+
+        _SetLeftInteractable();
+    }
+    #endregion
+
+    #region Set Interactable
+    /**this functions sets interactable the buttons
+     * to let the player have a visual feedback on which item is selected
+     * 
+     * Each buttons have a component "Event trigger" that listen to the event
+     * "on mouse enter" on the area of a slice of the wheel
+     * when the mouse enter the area it calls the corresponding function  (EnableXXXX)
+     * that enables the corresponding item
+     */
+    private void _ActivateWheel() {
+        top.interactable = true;
+        bottom.interactable = true;
+        left.interactable = true;
+        right.interactable = true;
+    }
+    private void _SetTopInteractable() {
+       top.interactable = true;
+        bottom.interactable = false;
+        left.interactable = false;
+        right.interactable = false;
+
+    }
+
+    private void _SetBottomInteractable() {
+        top.interactable = false;
+        bottom.interactable = true;
+        left.interactable = false;
+        right.interactable = false;
+    }
+
+    private void _SetLeftInteractable() {
+        top.interactable = false;
+        bottom.interactable = false;
+        left.interactable = true;
+        right.interactable = false;
+
+    }
+
+    private void _SetRightInteractable() {
+        top.interactable = false;
+        bottom.interactable = false;
+        left.interactable = false;
+        right.interactable = true;
+
     }
     #endregion
 }
