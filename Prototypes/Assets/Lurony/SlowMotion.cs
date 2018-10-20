22ccd06e3b3f8aa09f6b235a795804560420e6a9
Michael <michael.danielnaguib@gmail.com>
2018-10-20 12:44:58 +0200

    ADD:slowmotion  tutorial and implementation

---
+++ Prototypes/Assets/Lurony/SlowMotion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowMotion : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("PS4_L1") || Input.GetKeyDown(KeyCode.I)) { //https://bit.ly/2yQF6uM  Documentation link
+            Time.timeScale = .5f;   //halves the scale at which the time is passing
+        }
+
+        if (Input.GetButtonUp("PS4_L1") || Input.GetKeyUp(KeyCode.I)) {// restores the time scales
+            Time.timeScale = 1f;
+        }
+    }
+}
