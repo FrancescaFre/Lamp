6275386ff5f338ca99a9749dc3be81fbdcf6edfd
Michael <michael.danielnaguib@gmail.com>
2018-10-20 15:50:52 +0200

    Update comments

--- Prototypes/Assets/Learn Everything Fast/GravityBody.cs
+++ Prototypes/Assets/Learn Everything Fast/GravityBody.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof(Rigidbody))]  //this script requires a rigidbody to work
 public class GravityBody : MonoBehaviour {
+    
     public PlanetGravity AttractorPlanet;
 
-	// Use this for initialization
-	void Start () {
+	
+	void Awake () {
+       // AttractorPlanet = GameObject.FindObjectOfType<PlanetGravity>();   //in case the search happens at the beginning of the level
         GetComponent<Rigidbody>().useGravity = false;
-        GetComponent<Rigidbody>().constraints=RigidbodyConstraints.FreezeRotation;
+        GetComponent<Rigidbody>().constraints=RigidbodyConstraints.FreezeRotation;  //the rotation is done manually
 	}
 	
 	// Update is called once per frame
--- Prototypes/Assets/Learn Everything Fast/PlanetGravity.cs
+++ Prototypes/Assets/Learn Everything Fast/PlanetGravity.cs
@@ -2,28 +2,24 @@
 using UnityEngine;
 
 public class PlanetGravity : MonoBehaviour {
-    public float gravity=-12f;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public float gravity=-12f;  //gravity power
+
     /// <summary>
     /// Attracts the object toward the core of the planet
     /// </summary>
-    /// <param name="transform"></param>
+    /// <param name="transform"> The transform of the GameObject that needs to be attracted to the planet</param>
     public void Attract(Transform playerTransform) {
 
-        Vector3 gravityUp = (playerTransform.position - transform.position).normalized;
-        Vector3 localUp = playerTransform.up;
+      
 
-        playerTransform.GetComponent<Rigidbody>().AddForce(gravityUp * gravity);
+        // direction the rigidbody has to face
+        Vector3 targetDir = (playerTransform.position - transform.position).normalized; //it is the direction between the body and the center of the planet 
+        Vector3 bodyUp = playerTransform.up;   //rigidbody current direction
+        
+        playerTransform.GetComponent<Rigidbody>().AddForce(targetDir * gravity);
 
-        Quaternion targetRotation = Quaternion.FromToRotation(localUp, gravityUp) * playerTransform.rotation;
+        Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, targetDir) * playerTransform.rotation;
         playerTransform.rotation = Quaternion.Slerp(playerTransform.rotation, targetRotation, 50f * Time.deltaTime);
       
     }
