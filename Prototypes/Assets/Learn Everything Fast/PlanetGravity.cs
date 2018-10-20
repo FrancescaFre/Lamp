5c6ca7a4eb5ca8f1324ab18eb516866cb5e5c9de
Michael <michael.danielnaguib@gmail.com>
2018-10-20 15:09:43 +0200

    ADD: walk around planet tutorials
    it is now possible to walk around the planet with the keyboard and the joypad

---
+++ Prototypes/Assets/Learn Everything Fast/PlanetGravity.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+
+public class PlanetGravity : MonoBehaviour {
+    public float gravity=-12f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    /// <summary>
+    /// Attracts the object toward the core of the planet
+    /// </summary>
+    /// <param name="transform"></param>
+    public void Attract(Transform playerTransform) {
+
+        Vector3 gravityUp = (playerTransform.position - transform.position).normalized;
+        Vector3 localUp = playerTransform.up;
+
+        playerTransform.GetComponent<Rigidbody>().AddForce(gravityUp * gravity);
+
+        Quaternion targetRotation = Quaternion.FromToRotation(localUp, gravityUp) * playerTransform.rotation;
+        playerTransform.rotation = Quaternion.Slerp(playerTransform.rotation, targetRotation, 50f * Time.deltaTime);
+      
+    }
+}
---
+++ Prototypes/Assets/Learn Everything Fast/PlayerGravityBody.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGravityBody : MonoBehaviour {
+    public PlanetGravity AttractorPlanet;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().constraints=RigidbodyConstraints.FreezeRotation;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (AttractorPlanet)
+            this.AttractorPlanet.Attract(transform);
+	}
+}
