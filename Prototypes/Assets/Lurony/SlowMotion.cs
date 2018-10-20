36b898d7c5a82e61e6b16c3409ed2dbf6d677e2d
Michael <michael.danielnaguib@gmail.com>
2018-10-20 13:00:33 +0200

    Fix: comment clarification

--- Prototypes/Assets/Lurony/SlowMotion.cs
+++ Prototypes/Assets/Lurony/SlowMotion.cs
@@ -7,7 +7,7 @@ public class SlowMotion : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetButton("PS4_L1") || Input.GetKey(KeyCode.I)) { //https://bit.ly/2yQF6uM  Documentation link
-            Time.timeScale = .5f;   //halves the scale at which the time is passing
+            Time.timeScale = .5f;   //halves the scale at which the time is passing as long as the button is held down
         }
         else {// restores the time scales
             Time.timeScale = 1f;
