8a289256ab0501600f7877554527048601ca6187
Nicolò Cavedoni <o91vexac@gmail.com>
2018-10-22 17:22:07 +0200

    ADD: Hummelwalker Gravity
    Added Hummelwalker Gravitation Tutorial + sample scene

---
+++ Prototypes/Assets/Hummelwalker/Gravity.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+
+    float gravityforce = -20;
+
+    public void ApplyGravity(Transform receiver)
+    {
+        Rigidbody rb = receiver.GetComponent<Rigidbody>();
+        Vector3 forceUp = receiver.position - transform.position;
+        Vector3 dir;
+        dir = gravityforce * forceUp.normalized;
+
+        rb.AddForce(dir);
+        Vector3 receiverUp = receiver.up;
+
+        Quaternion rot = Quaternion.FromToRotation(receiverUp, forceUp) * receiver.rotation;
+
+        receiver.rotation = rot;
+    }
+}
