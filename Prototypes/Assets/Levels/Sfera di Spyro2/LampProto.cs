326a644dab9d713441c23b7600d4d6a386bdf300
Nicolò Cavedoni <o91vexac@gmail.com>
2018-10-22 21:58:23 +0200

    FIX: Livello Carino ultimato
    /prototype/levels/Sfera di Spyro2
    
    è un livellino di merda ma giusto per provare lo script della gravità tedesco coi salti

---
+++ Prototypes/Assets/Levels/Sfera di Spyro2/LampProto.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampProto : MonoBehaviour {
+
+    public List<Material> materials;
+
+    void Start()
+    {
+        gameObject.GetComponent<MeshRenderer>().material = materials[0];
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        // Quando ho fatto un altro playercontroller dava problemi, quindi ho rinominato il mio.
+        // Questa versione di script usa questo (ma commentata sotto c'è l'originale)
+
+        if (other.GetComponent<PlayerControllerHummel>())
+            gameObject.GetComponent<MeshRenderer>().material = materials[1];
+
+        /*
+        if (other.GetComponent<PlayerController>())
+            gameObject.GetComponent<MeshRenderer>().material = materials[1];
+        */
+    }
+}
