3aec68cd128a7f5db7766e5f97705e0f13486837
Michael <michael.danielnaguib@gmail.com>
2018-10-26 21:10:34 +0200

    ADD: stub implementation of the use of a skill

--- Main_Project/Assets/Scripts/Characters/CharacterControllerM.cs
+++ Main_Project/Assets/Scripts/Characters/CharacterControllerM.cs
@@ -1,27 +1,58 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(ItemWheel))]
 public class CharacterControllerM : MonoBehaviour {
     public float movementSpeed = 18f;
-    
+    public Status curseStatus { get; private set; };
+
 
     private Rigidbody _rig;
-	// Use this for initialization
-	void Start () {
+
+    void Awake() {
+        curseStatus = Status.NORMAL;  
+    }
+
+    // Use this for initialization
+    void Start () {
         _rig = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        this.CheckMovement();
+
+        this.CheckSkillInteraction();
+    }
+    /// <summary>
+    /// Moves the player if an input is detected
+    /// </summary>
+    private void CheckMovement() {
         //to move the player
         float horiz_axis = Input.GetAxis("Horizontal");
         float vert_axis = Input.GetAxis("Vertical");
 
-        Vector3 movement = transform.TransformDirection(new Vector3(horiz_axis,0,vert_axis)*movementSpeed*Time.deltaTime);
+        Vector3 movement = transform.TransformDirection(new Vector3(horiz_axis, 0, vert_axis) * movementSpeed * Time.deltaTime);
 
-        _rig.MovePosition(transform.position+movement);
-
+        _rig.MovePosition(transform.position + movement);
+    }
+    
+    /// <summary>
+    /// The skill is used if an input is detected
+    /// </summary>
+    private  void CheckSkillInteraction() {
+        if (Input.GetButtonDown("PS4_Button_O") || Input.GetKeyDown(KeyCode.Q)) {
+            Debug.Log("SKILL used");
+        }
     }
 
-
+    /// <summary>
+    /// Change the state of the curse of the character
+    /// </summary>
+    /// <param name="stat">New state of the curse (ENUM)</param>
+    public void ChangeStatus(Status stat) {
+        this.curseStatus = stat;
+    }
 }
+
+public enum Status { NORMAL=0, HALF_CURSED, CURSED }
