e2ed53336f298c81c949d67a696e3ca1789fe90a
Michael <michael.danielnaguib@gmail.com>
2018-10-27 10:56:26 +0200

    ADD: CameraManager and testing materials

---
+++ Main_Project/Assets/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraManager : MonoBehaviour {//http://youttu.be/MFQhpwc6cKE
+    public Transform playerModel;// the model of the player, NOT the pivot
+
+    [Range(0,1)]
+    public float smoothSpeed=.125f;   //the higher it is, the faster thecamera will lock on the player
+
+    public Vector3 offset;
+
+
+
+    void FixedUpdate() {
+        Vector3 desiredPosition = playerModel.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed/*Time.deltaTime*/);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(playerModel);
+    }
+
+}
