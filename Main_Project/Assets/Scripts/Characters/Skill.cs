0c9f58a242a2478294eeefffb271220b558c204a
Bassi Francesca <bassi.francesca95@gmail.com>
2018-11-10 15:46:38 +0100

    Commit to merge something

--- Main_Project/Assets/Scripts/Characters/Skill.cs
+++ Main_Project/Assets/Scripts/Characters/Skill.cs
@@ -4,7 +4,45 @@ using UnityEngine;
 
 public class Skill : MonoBehaviour {
 
-    public enum AgeChar {PREHISTORY= 0, ORIENTAL, VICTORIAN, FUTURE}
+    public enum AgeChar { PREHISTORY = 0, ORIENTAL, VICTORIAN, FUTURE }
+    private AgeChar typeOfCharacter;
+
+    public Dictionary<AgeChar, string> Skills = new Dictionary<AgeChar, string>();
+    private PlayerController pc;
+
+    private Robot robot;
+    private Mimic mimic;
+    private Painter painter;
+
+    private void Start()
+    {
+        Skills.Add(AgeChar.PREHISTORY, "ActiveDash");
+        Skills.Add(AgeChar.ORIENTAL, "ActiveMimic");
+        Skills.Add(AgeChar.VICTORIAN, "ActivePainter");
+        Skills.Add(AgeChar.FUTURE, "ActiveRobot");
+
+        pc = GetComponent<PlayerController>();
+        //get enum from the pc when the enum will be in the playercontroller
+        typeOfCharacter = AgeChar.FUTURE;
+
+        robot = GetComponentInChildren<Robot>();
+        mimic = GetComponent<Mimic>();
+        painter = GetComponent<Painter>();
+    }
+
+    public void ActiveSkill()
+    {
+        if (robot)
+            robot.ActivateSkill();
+        else if (mimic)
+        else if (painter)
+
+    }
     
+
+    public void DectivateSkill() {
+
+    }
+
 
 }
