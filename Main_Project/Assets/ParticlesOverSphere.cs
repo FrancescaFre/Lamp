a3a0b2c71ccee8fe0e0b97600755719fd53af304
Bassi Francesca <bassi.francesca95@gmail.com>
2018-12-01 21:40:49 +0100

    ADD: Itween plugin, particles

---
+++ Main_Project/Assets/ParticlesOverSphere.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class ParticlesOverSphere : MonoBehaviour {
+
+    public ParticleSystem ps;
+    
+	// Use this for initialization
+	void Start () {
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ps.Emit(new ParticleSystem.EmitParams() { position = Random.onUnitSphere*21}, 1);
+
+	}
+}
