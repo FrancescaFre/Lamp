c2030cde2686540057eaf271a439b3035fbe3094
Nicolò Cavedoni <o91vexac@gmail.com>
2018-12-12 01:18:10 +0100

    ADD: Zooming Camera

--- Main_Project/Assets/Scripts/Managers/CameraManager.cs
+++ Main_Project/Assets/Scripts/Managers/CameraManager.cs
@@ -2,9 +2,20 @@
 
 public class CameraManager : MonoBehaviour {
 
-    //[Tooltip("The dummy player's camera transform (must be dummy's child)")]
-    private Transform _dummyCam; // The dummy camera is used to avoid 3-dimensional inverse revolutions
+    [Tooltip("Minimum field of view reached zooming in")]
+    public float minZoom = 15f;
 
+    [Tooltip("Maximum field of view reached zooming out")]
+    public float maxZoom = 90f;
+
+    [Tooltip("How fast are min and max reached")]
+    [Range(15f, 25f)]
+    public float sensitivity = 20f;
+
+    [Tooltip("True to zoom out scrolling up and vice-versa")]
+    public bool inverseZoom = false;
+
+    private Transform _dummyCam; // The dummy camera is used to avoid 3-dimensional inverse revolutions
     private Vector3 _camOffset; // Difference in position between the main camera and the dummy one
     private float yPosition = 0; // Position on Y of the rotating camera (starts from zero)
 
@@ -14,13 +25,21 @@ public class CameraManager : MonoBehaviour {
         AlignCameras();
         _camOffset = transform.position - _dummyCam.position; 
     }
-   
+
     // Rotates the camera as the right analog stick is pressed
     private void FixedUpdate()
     {
         yPosition += Input.GetAxis("Mouse X") * Time.deltaTime * 180f; // TESTING
         //yPosition += Input.GetAxis("RightStick X") * Time.deltaTime * 90f; // Real one with the joypad analog stick
         _dummyCam.parent.localRotation = Quaternion.Euler(_dummyCam.parent.localRotation.x, yPosition, _dummyCam.parent.localRotation.z);
+
+        float fov = Camera.main.fieldOfView;
+        if (inverseZoom)
+            fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+        else
+            fov -= Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+        fov = Mathf.Clamp(fov, minZoom, maxZoom);
+        Camera.main.fieldOfView = fov;
     }
 
     // Every frame takes the same position as the dummy camera, in relation to the player
