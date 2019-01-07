d93d9570fe9c8ae9ba6913a946966002091b41a1
Michael <michael.danielnaguib@gmail.com>
2019-01-07 18:15:37 +0100

    ADD: color lerp
    FIX: materials colors of new chars
    UPDATE: ingame canvas

---
+++ Main_Project/Assets/Scripts/Tools/ColorChanger.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour {
+    public Light[] allLights;
+    public Color endColor;
+    public List<ColorLerp> lerpers;
+    public float fadeTime;
+
+
+	
+	void Start () {
+        allLights = GetComponentsInChildren<Light>();
+        lerpers = new List<ColorLerp>(allLights.Length);
+
+
+        for (int i = 0; i < allLights.Length; i++) {
+            lerpers.Add(new ColorLerp(allLights[i], allLights[i].color, endColor,fadeTime));
+        }
+        //eOwner = GetComponent<Enemy>();
+	}
+    /*
+    private void FixedUpdate() {
+        if (eOwner)
+            if ((eOwner.currentStatus == EnemyStatus.SEARCHING || eOwner.currentStatus == EnemyStatus.SEEKING) && Lock) {
+                if (previusState == EnemyStatus.SEARCHING) return;        // if the enemy sees the player again while in seeking
+                Debug.Log("LERP COLOR "+eOwner.currentStatus);
+                this.Lerp();
+                Lock = false;
+                previusState = eOwner.currentStatus;
+            }
+            else if (eOwner.currentStatus == EnemyStatus.RETURN && Lock) {
+                Debug.Log("REVERSE LERP COLOR " + eOwner.currentStatus);
+                this.ReverseLerp();
+                Lock = false;
+                previusState = eOwner.currentStatus;
+            }
+            else if ((eOwner.currentStatus == EnemyStatus.WANDERING || eOwner.currentStatus == EnemyStatus.SEARCHING) && !Lock) {
+                Debug.Log("RELEASE LOCK" + eOwner.currentStatus);
+                Lock = true;
+                previusState = eOwner.currentStatus;
+            }
+    }
+    */
+    public void Lerp() {
+        for (int i = 0; i < lerpers.Count; i++) {
+            lerpers[i].LerpColors();
+        }
+    }
+
+    public void ReverseLerp() {
+        for (int i = 0; i < lerpers.Count; i++) {
+            lerpers[i].ReverseLerpColors();
+        }
+    }
+
+
+
+
+}
---
+++ Main_Project/Assets/Scripts/Tools/ColorLerp.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using MEC;
+
+public class ColorLerp  {
+    private Light lightSource;
+    public Color startColor;
+    public Color endColor;
+    public float fadeTime;
+
+    public ColorLerp(Light lit, Color startColor, Color endColor,float fadeTime) {
+        lightSource = lit;
+        this.startColor = startColor;
+        this.endColor = endColor;
+        this.fadeTime = fadeTime;
+    }
+
+    public void LerpColors() {
+        if (!lightSource) return;
+        Timing.RunCoroutine(_LerpAtoB());
+    }
+
+    private IEnumerator<float> _LerpAtoB() {
+
+        for (float t = 0.01f; t < fadeTime; t+=0.1f) {
+            lightSource.color = Color.Lerp(startColor,endColor,t/fadeTime);
+            yield return Timing.WaitForOneFrame;
+        }
+    }
+
+    public void ReverseLerpColors() {
+        if (!lightSource) return;
+        Timing.RunCoroutine(_LerpBtoA());
+    }
+
+    private IEnumerator<float> _LerpBtoA() {
+
+        for (float t = 0.01f; t < fadeTime; t+=0.1f) {
+            lightSource.color = Color.Lerp(endColor, startColor, t/fadeTime);
+            yield return Timing.WaitForOneFrame;
+        }
+    }
+
+
+}
