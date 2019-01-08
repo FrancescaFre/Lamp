b2c591e871f8e19506ecf9e6594004b544887408
Bassi Francesca <bassi.francesca95@gmail.com>
2019-01-08 05:46:53 +0100

    fix in tutorial

--- Main_Project/Assets/Scripts/Characters/PlayerController.cs
+++ Main_Project/Assets/Scripts/Characters/PlayerController.cs
@@ -46,7 +46,7 @@ public class PlayerController : MonoBehaviour {
     public bool runningAnimation = false;
 
     private Rigidbody _rb;
-    private int _missingParts = 0;
+    public int _missingParts = 0;
     public int keys = 0;
 
     public ParticleSystem halfCurseEffect;
