182e55358e184ada95a22ba97319ded19c76504d
Michael <michael.danielnaguib@gmail.com>
2019-01-30 01:50:35 +0100

    FIX: Volumetric light in GAME mode
    ADD: increase the light when the fog dissipate

--- Main_Project/Assets/WorldEnlighter.cs
+++ Main_Project/Assets/WorldEnlighter.cs
@@ -1,14 +1,15 @@
 ﻿using UnityEngine;
 
 public class WorldEnlighter : MonoBehaviour {
-
+    
     private Light _worldLight;
     private float _lerpStep;
     private VolumetricLight _fog;
     [Tooltip("TRUE if you only want to change the color of the fog.")]
     public bool changeColor = false;
     public Color endColor = Color.white;
-
+    public Color ambientLightIncrease;
+    
     // Use this for initialization
     void Start() {
         _worldLight = GetComponent<Light>();
@@ -16,13 +17,26 @@ public class WorldEnlighter : MonoBehaviour {
         _fog = GetComponent<VolumetricLight>();
         _lerpStep = 1f / GameManager.Instance.levelLoaded.allyLamps;
         _fog.ScatteringCoef = GameManager.Instance.levelLoaded.allyLamps / 10f;
+
+        float temp = 50f / 255f;
+        RenderSettings.ambientLight = new Color(temp, temp, temp);
+        temp = (30f / GameManager.Instance.levelLoaded.allyLamps)/255f;
+
+        ambientLightIncrease = new Color(temp, temp, temp);
     }
 
     public void Enlight() {
         if (changeColor)
             _worldLight.color = Color.Lerp(_worldLight.color, endColor, _lerpStep);
-        else
+        else {
             _fog.ScatteringCoef -= .1f;
+            RenderSettings.ambientLight += ambientLightIncrease;
+        }
+        
+
+
+
+
     }
 
     private void Update() {//test
