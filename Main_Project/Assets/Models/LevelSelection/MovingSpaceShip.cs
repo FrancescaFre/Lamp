bf1b8767ae5e57dd2a9638ee8ede159a25f8a271
Francesca <bassi.francesca95@gmail.com>
2019-06-20 23:46:06 +0200

    add new level selection

---
+++ Main_Project/Assets/Models/LevelSelection/MovingSpaceShip.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingSpaceShip : MonoBehaviour {
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+
+    public ParticleSystem [] fire = new ParticleSystem [2];
+
+    private Rigidbody rb;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = this.GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        float translation = Input.GetAxis("Vertical") * speed;
+
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        if (translation != 0.0)
+        {
+            foreach (ParticleSystem ps in fire)
+                if (!ps.gameObject.activeInHierarchy)
+                {
+                    ps.gameObject.SetActive(true);
+                    ps.Play();
+                }
+        }
+        else if (translation == 0.0)
+            foreach (ParticleSystem ps in fire)
+                if (ps.isPlaying)
+                    ps.Stop();
+                else
+                    if (!ps.isEmitting)
+                    ps.gameObject.SetActive(false);
+
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+    }
+}
