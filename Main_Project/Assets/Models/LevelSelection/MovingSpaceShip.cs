a16fad0bae28f0e9ec8c5fdbcfee09101ca4052a
Francesca <bassi.francesca95@gmail.com>
2019-06-21 12:32:30 +0200

    add: camera ship

--- Main_Project/Assets/Models/LevelSelection/MovingSpaceShip.cs
+++ Main_Project/Assets/Models/LevelSelection/MovingSpaceShip.cs
@@ -3,16 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingSpaceShip : MonoBehaviour {
-    public float speed = 10.0f;
+    public float speed = 15.0f;
     public float rotationSpeed = 100.0f;
+
+    private float minX= -100;
+    private float maxX = 60;
+    private float minZ = -100;
+    private float maxZ = 60;
 
     public ParticleSystem [] fire = new ParticleSystem [2];
 
     private Rigidbody rb;
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Planet/Switch"))
+            speed -= 10; 
+    }
 
-	// Use this for initialization
-	void Start () {
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.CompareTag("Planet/Switch"))
+            speed += 10; 
+    }
+
+
+    // Use this for initialization
+    void Start () {
         rb = this.GetComponent<Rigidbody>();
 	}
 
@@ -43,6 +60,11 @@ public class MovingSpaceShip : MonoBehaviour {
         rotation *= Time.deltaTime;
 
         transform.Translate(0, 0, translation);
+
+        float posx = Mathf.Clamp(transform.position.x, -minX, minX);
+        float posz = Mathf.Clamp(transform.position.z, -minZ, minZ);
+        transform.position = new Vector3(posx, 0, posz);
+
         transform.Rotate(0, rotation, 0);
     }
 }
